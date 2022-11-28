ed05d88ef99783b270732962d6d2224e2c89f2a4
phylis13 <phylisnjeri54@gmail.com>
2022-11-28 21:03:28 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solutions in this file
+function writeCards(names, message){
+    let messages = [];
+    for(let i = 0; i < names.length; i++){
+        messages.push(`Thank you, ${names[i]}, for the wonderful ${message} gift!`);
+    }
+    return messages;
+}
+
+function countDown(n){
+    for (let i = n; i > -1; i--){
+        console.log(i);
+    }
+}
